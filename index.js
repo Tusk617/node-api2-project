018ed424f5b0bdc6fec1ebc1ebb97aa44c3f8fe6
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-04 15:34:56 -0400

    Completed stretch

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 
 const server = express();
@@ -6,8 +7,18 @@ const server = express();
 server.use(express.json());
 
 const postsRouter = require("./posts/posts-router.js");
+// const jokesRouter = require("./jokes/jokes_router.js");
 
 server.use("/api/posts", postsRouter);
+// server.use("/api/jokes", jokesRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({
+        port: process.env.PORT,
+        environment: process.env.NODE_ENV,
+        joke: process.env.FUNNY_JOKE
+    })
+})
 
 
 const port = process.env.PORT || 8000;
---
+++ jokes/jokes_router.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//     res.status(200).json({
+//         port: process.env.PORT,
+//         environment: process.env.NODE_ENV,
+//         joke: process.env.FUNNY_JOKE
+//     })
+// })
